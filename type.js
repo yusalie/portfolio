17efd04a3f9bad7a3c8aa69531fab52d5a001605
yusalie <71071393+yusalie@users.noreply.github.com>
2021-02-03 16:08:02 +0200

    Add files via upload

---
+++ type.js
@@ -0,0 +1,18 @@
+let i = 0;
+let j = 0;
+let h1 = 'Welcome To my website!';
+let h2 = 'My name is Yusuf Salie'
+let txtSpd = 50;
+let txtpd2 = 1000*10;
+function writeTxt() {
+  if (i < h1.length) {
+    document.getElementById("h1").innerHTML += h1.charAt(i);
+    i++;
+    setTimeout(writeTxt, txtSpd);
+  }
+  if (j < h2.length) {
+    document.getElementById("h2").innerHTML += h2.charAt(j);
+    j++;
+    setTimeout(writeTxt, txtSpd2);
+  }
+}
