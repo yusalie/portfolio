ebbb518c8e925fdb4ee7affed61869c0850c5f7e
yusalie <71071393+yusalie@users.noreply.github.com>
2021-05-13 13:49:04 +0200

    Update type.js

--- type.js
+++ type.js
@@ -1,7 +1,7 @@
 let i = 0;
 let j = 0;
-let h1 = 'Yusuf Salie';
-let h2 = 'Aspiring back-end developer'
+let h1 = "Yusuf Salie";
+let h2 = "Aspiring back-end developer";
 let txtSpd = 50;
 let txtpd2 = 1000*10;
 function writeTxt() {
